c6f1fb97d4496f976537cf4600a6a3ebe36e42e3
nicklanng <rinse@rinseworld.com>
2018-02-21 22:42:56 +0000

    Vendored uuid package has different API

--- api/handlers/ensurerequestid.go
+++ api/handlers/ensurerequestid.go
@@ -14,7 +14,7 @@ func EnsureRequestID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Header.Get("x-request-id") == "" {
-			requestID := uuid.Must(uuid.NewV4())
+			requestID := uuid.NewV4()
 			r.Header.Add("x-request-id", requestID.String())
 		}
 
